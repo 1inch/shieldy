969203ed8f3d4199df0fa414ee654e98b095fcdc
backmeupplz <backmeupplz@gmail.com>
2018-11-07 02:40:07 -0800

    fix lock

--- src/commands/lock.ts
+++ src/commands/lock.ts
@@ -4,10 +4,10 @@ import { strings } from '../helpers/strings'
 import { checkLock } from '../middlewares/checkLock'
 
 export function setupLock(bot: Telegraf<ContextMessageUpdate>) {
-  bot.command('lock', checkLock, ctx => {
+  bot.command('lock', checkLock, async ctx => {
     let chat = ctx.dbchat
     chat.adminLocked = !chat.adminLocked
-    chat = (chat as any).save()
+    chat = await (chat as any).save()
     ctx.replyWithMarkdown(
       strings(ctx.dbchat, chat.adminLocked ? 'lock_true' : 'lock_false'),
       Extra.inReplyTo(ctx.message.message_id)
