514f2227c1304e94da4dbe6699a55533f24f255d
backmeupplz <backmeupplz@gmail.com>
2018-11-07 01:58:55 -0800

    fix for self enter, bot enter and disable newcomers for now

--- src/helpers/newcomers.ts
+++ src/helpers/newcomers.ts
@@ -1,13 +1,20 @@
 // Dependencies
 import Telegraf, { ContextMessageUpdate } from 'telegraf'
+import { strings } from './strings'
 
 export function setupNewcomers(bot: Telegraf<ContextMessageUpdate>) {
   // Add newcomers
   bot.on('new_chat_members', ctx => {
     for (const member of ctx.message.new_chat_members) {
-      ctx.reply(
-        'Пожалуйста, пришлите любое сообщение в этот чат в течение 60 секунд или вы будете забанены'
-      )
+      if (member.username === (bot as any).options.username) {
+        ctx.replyWithMarkdown(strings(ctx.dbchat, 'help'), {
+          disable_web_page_preview: true,
+        })
+        continue
+      }
+      if (member.is_bot) {
+        continue
+      }
     }
   })
   // Check newcomers
