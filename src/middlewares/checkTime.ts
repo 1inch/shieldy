3dc070643f810cf0bb8879d5c79bd8cec19e6ec9
backmeupplz <backmeupplz@gmail.com>
2018-11-14 18:20:39 -0800

    fix check time

--- src/middlewares/checkTime.ts
+++ src/middlewares/checkTime.ts
@@ -3,14 +3,14 @@ import { ContextMessageUpdate } from 'Telegraf'
 export async function checkTime(ctx: ContextMessageUpdate, next: () => any) {
   switch (ctx.updateType) {
     case 'message':
-      if (new Date().getTime() / 1000 - ctx.message.date > 5 * 60) {
+      if (new Date().getTime() / 1000 - ctx.message.date < 5 * 60) {
         next()
       }
       break
     case 'callback_query':
       if (
         ctx.callbackQuery.message &&
-        new Date().getTime() / 1000 - ctx.callbackQuery.message.date > 5 * 60
+        new Date().getTime() / 1000 - ctx.callbackQuery.message.date < 5 * 60
       ) {
         next()
       }
