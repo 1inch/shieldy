eeb94aa8a68df885c7ec5f54cc698f7147af0856
backmeupplz <backmeupplz@gmail.com>
2018-11-24 16:31:56 -0800

    change keyboard back

--- src/commands/language.ts
+++ src/commands/language.ts
@@ -12,16 +12,12 @@ export function setupLanguage(bot: Telegraf<ContextMessageUpdate>) {
       Extra.webPreview(false)
         .inReplyTo(ctx.message.message_id)
         .markup(m =>
-          m.inlineKeyboard(
-            [
-              m.callbackButton('English', 'en'),
-              m.callbackButton('Русский', 'ru'),
-            ],
-            [
-              m.callbackButton('Italiano', 'it'),
-              m.callbackButton('Eesti', 'et'),
-            ]
-          )
+          m.inlineKeyboard([
+            m.callbackButton('English', 'en'),
+            m.callbackButton('Русский', 'ru'),
+            m.callbackButton('Italiano', 'it'),
+            m.callbackButton('Eesti', 'et'),
+          ])
         )
     )
   })
