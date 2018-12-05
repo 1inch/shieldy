a2815f990459986a604fa34f1b041cda5f48c436
backmeupplz <backmeupplz@gmail.com>
2018-12-04 23:00:49 -0800

    add more restricts

--- src/middlewares/checkRestrict.ts
+++ src/middlewares/checkRestrict.ts
@@ -16,7 +16,9 @@ export async function checkRestrict(
     ((ctx.message.entities &&
       ctx.message.entities.length &&
       ctx.message.entities.map(e => e.type).indexOf('url') > -1) ||
-      ctx.message.forward_from)
+      (ctx.message.forward_from ||
+        ctx.message.forward_date ||
+        ctx.message.forward_from_chat))
   ) {
     try {
       await ctx.telegram.deleteMessage(ctx.chat.id, ctx.message.message_id)
