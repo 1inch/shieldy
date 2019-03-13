25941a0bd371cdb6e1cb41d9cabd78b1146e57c4
backmeupplz <backmeupplz@gmail.com>
2019-03-12 19:37:38 -0700

    try adding more time limits

--- src/commands/timeLimit.ts
+++ src/commands/timeLimit.ts
@@ -4,7 +4,7 @@ import { strings } from '../helpers/strings'
 import { checkIfFromReplier } from '../middlewares/checkIfFromReplier'
 import { checkLock } from '../middlewares/checkLock'
 
-const options = ['60', '120', '240']
+const options = [['10', '20', '30'], ['60', '120', '240']]
 
 export function setupTimeLimit(bot: Telegraf<ContextMessageUpdate>) {
   bot.command('timeLimit', checkLock, ctx => {
@@ -12,28 +12,34 @@ export function setupTimeLimit(bot: Telegraf<ContextMessageUpdate>) {
       strings(ctx.dbchat, 'time_limit'),
       Extra.inReplyTo(ctx.message.message_id).markup(m =>
         m.inlineKeyboard(
-          options.map(o =>
-            m.callbackButton(`${o} ${strings(ctx.dbchat, 'seconds')}`, o)
+          options.map(a =>
+            a.map(o =>
+              m.callbackButton(`${o} ${strings(ctx.dbchat, 'seconds')}`, o)
+            )
           )
         )
       )
     )
   })
 
-  bot.action(options, checkIfFromReplier, async ctx => {
-    let chat = ctx.dbchat
-    chat.timeGiven = Number(ctx.callbackQuery.data)
-    chat = await (chat as any).save()
-    const message = ctx.callbackQuery.message
+  bot.action(
+    options.reduce((p, c) => p.concat(c), []),
+    checkIfFromReplier,
+    async ctx => {
+      let chat = ctx.dbchat
+      chat.timeGiven = Number(ctx.callbackQuery.data)
+      chat = await (chat as any).save()
+      const message = ctx.callbackQuery.message
 
-    ctx.telegram.editMessageText(
-      message.chat.id,
-      message.message_id,
-      undefined,
-      `${strings(chat, 'time_limit_selected')} (${chat.timeGiven} ${strings(
-        chat,
-        'seconds'
-      )})`
-    )
-  })
+      ctx.telegram.editMessageText(
+        message.chat.id,
+        message.message_id,
+        undefined,
+        `${strings(chat, 'time_limit_selected')} (${chat.timeGiven} ${strings(
+          chat,
+          'seconds'
+        )})`
+      )
+    }
+  )
 }
