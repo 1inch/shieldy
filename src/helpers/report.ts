8b90c38293572548bcd49f042d3742a62a235c15
backmeupplz <backmeupplz@gmail.com>
2019-11-04 19:25:19 -0800

    add logs

--- src/helpers/report.ts
+++ src/helpers/report.ts
@@ -14,9 +14,9 @@ export async function report(
   try {
     await bot.telegram.sendMessage(
       adminChatId,
-      `<code>${error.message}</code>\n\n<code>${
-        error.stack
-      }</code>\n\n${error as any}`,
+      `<code>${error.message}</code>\n\n<code>${error.stack}</code>\n\n<code>${
+        (error as any).response
+      }</code>\n\n<code>${(error as any).parameters}</code>`,
       {
         parse_mode: 'HTML',
       } as any
