8ee36d0d6f7c884b97cf90da335992bab7d256ad
backmeupplz <backmeupplz@gmail.com>
2019-11-26 19:35:28 -0800

    fix cas

--- src/helpers/cas.ts
+++ src/helpers/cas.ts
@@ -1,8 +1,6 @@
 import axios from 'axios'
 
 export async function checkCAS(userId: number) {
-  const result = await axios(
-    `https://combot.org/api/cas/check?user_id=${userId}`
-  )
+  const result = await axios(`https://api.cas.chat/check?user_id=${userId}`)
   return !result.data.ok
 }
