c9772455496747e0265d5b244be778632a2920e6
backmeupplz <backmeupplz@gmail.com>
2020-01-11 06:53:55 -0800

    add turning off of the greeting message

--- src/commands/deleteGreetingTime.ts
+++ src/commands/deleteGreetingTime.ts
@@ -11,11 +11,17 @@ export function setupDeleteGreetingTime(bot: Telegraf<ContextMessageUpdate>) {
       +ctx.message.text.substr(19).trim() ||
       +ctx.message.text.substr(20 + (bot as any).options.username.length).trim()
     if (!isNaN(limitNumber) && limitNumber > 0 && limitNumber < 100000) {
-      let chat = ctx.dbchat
-      chat.deleteGreetingTime = limitNumber
-      chat = await chat.save()
+      ctx.dbchat.deleteGreetingTime = limitNumber
+      await ctx.dbchat.save()
       ctx.reply(
         strings(ctx.dbchat, 'greetsUsers_message_accepted'),
+        Extra.inReplyTo(ctx.message.message_id) as ExtraReplyMessage
+      )
+    } else {
+      ctx.dbchat.deleteGreetingTime = undefined
+      await ctx.dbchat.save()
+      ctx.reply(
+        `${strings(ctx.dbchat, 'greetsUsers_message_accepted')} 0`,
         Extra.inReplyTo(ctx.message.message_id) as ExtraReplyMessage
       )
     }
