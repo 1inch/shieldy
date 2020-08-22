caf0ca3c02bd4290e5af930c4fdfceeeccf54eac
backmeupplz <backmeupplz@gmail.com>
2020-08-21 17:06:02 -0700

    add todorant to captcha

--- src/helpers/newcomers.ts
+++ src/helpers/newcomers.ts
@@ -29,6 +29,9 @@ import { checkSuperAdmin } from '../middlewares/checkSuperAdmin'
 const kickedIds = {} as { [index: number]: number[] }
 const buttonPresses = {} as { [index: string]: boolean }
 
+const todorantAddition =
+  'Powered by <a href="https://todorant.com">Todorant</a> (<a href="https://t.me/borodutch_support/110">?</a>)'
+
 export function setupNewcomers(bot: Telegraf<ContextMessageUpdate>) {
   bot.command('greetMe', checkSuperAdmin, greetUser)
   bot.on('new_chat_members', checkIfGroup, onNewChatMembers)
@@ -461,19 +464,21 @@ async function notifyCandidate(
         .replace(/\$seconds/g, `${chat.timeGiven}`)
       if (image) {
         return ctx.replyWithPhoto({ source: image.png } as any, {
-          caption: textToSend,
+          caption: `${textToSend}\n${todorantAddition}`,
           parse_mode: 'HTML',
         })
       } else {
         return ctx.telegram.sendMessage(
           chat.id,
-          textToSend,
+          `${textToSend}\n${todorantAddition}`,
           extra as ExtraReplyMessage
         )
       }
     } else {
       const message = cloneDeep(captchaMessage.message)
-      message.text = `${getUsername(candidate)}\n\n${message.text}`
+      message.text = `${getUsername(candidate)}\n\n${
+        message.text
+      }\n${todorantAddition}`
       try {
         const sentMessage = await ctx.telegram.sendCopy(
           chat.id,
@@ -499,7 +504,7 @@ async function notifyCandidate(
         )}</a>${warningMessage} (${chat.timeGiven} ${strings(
           chat,
           'seconds'
-        )})`,
+        )})\n${todorantAddition}`,
         parse_mode: 'HTML',
       })
     } else {
@@ -513,7 +518,7 @@ async function notifyCandidate(
         )}</a>${warningMessage} (${chat.timeGiven} ${strings(
           chat,
           'seconds'
-        )})`,
+        )})\n${todorantAddition}`,
         extra
       )
     }
