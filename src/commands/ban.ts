92a247782172bfb032ee062b44daeea2394b21c4
backmeupplz <backmeupplz@gmail.com>
2020-09-08 11:25:37 -0700

    fix ban bug

--- src/commands/ban.ts
+++ src/commands/ban.ts
@@ -6,12 +6,10 @@ import { report } from '../helpers/report'
 
 export function setupBan(bot: Telegraf<ContextMessageUpdate>) {
   bot.command('ban', checkLock, async (ctx) => {
-    console.log(1)
     // Check if reply
     if (!ctx.message || !ctx.message.reply_to_message) {
       return
     }
-    console.log(2)
     // Get replied
     const repliedId = ctx.message.reply_to_message.from.id
     // Check if sent by admin
@@ -19,17 +17,13 @@ export function setupBan(bot: Telegraf<ContextMessageUpdate>) {
     if (!admins.map((a) => a.user.id).includes(ctx.from.id)) {
       return
     }
-    console.log(3)
     // Check permissions
     const admin = admins.find((v) => v.user.id === ctx.from.id)
-    console.log(admin)
-    if (!admin.can_restrict_members) {
+    if (admin.status !== 'creator' && !admin.can_restrict_members) {
       return
     }
-    console.log(4)
     // Ban in Telegram
     await ctx.telegram.kickChatMember(ctx.dbchat.id, repliedId)
-    console.log(5)
     // Unrestrict in shieldy
     ctx.dbchat.restrictedUsers = ctx.dbchat.restrictedUsers.filter(
       (c) => c.id !== repliedId
