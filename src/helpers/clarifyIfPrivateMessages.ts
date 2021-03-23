b5b0510695be40efa5e082a890a129c37f55178a
backmeupplz <backmeupplz@gmail.com>
2021-03-23 08:43:23 -0700

    don't delete pm messages

--- src/helpers/clarifyIfPrivateMessages.ts
+++ src/helpers/clarifyIfPrivateMessages.ts
@@ -1,4 +1,3 @@
-import { addMessageToDelete } from '@models/MessageToDelete'
 import { strings } from '@helpers/strings'
 import { Context } from 'telegraf'
 
@@ -6,9 +5,6 @@ export async function clarifyIfPrivateMessages(ctx: Context, next: Function) {
   if (ctx.chat?.type !== 'private') {
     return next()
   }
-  const sent = await ctx.reply(strings(ctx.dbchat, 'commandsInPrivateWarning'))
-  const deleteTime = new Date()
-  deleteTime.setSeconds(deleteTime.getSeconds() + 30)
-  addMessageToDelete(sent.chat.id, sent.message_id, deleteTime)
+  await ctx.reply(strings(ctx.dbchat, 'commandsInPrivateWarning'))
   return next()
 }
